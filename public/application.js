13781011b84a96cd8eb8287f7503d63724010329
Tori Versteegh <torivers@Toris-MacBook-Air-2.local>
2023-11-09 12:08:14 -0500

    adding files

---
+++ public/application.js
@@ -0,0 +1,29 @@
+
+function changeContent(id) {
+  // HTTP request
+  //  console.log("testing");
+    var xhttp = new XMLHttpRequest();
+    let url = `/application/character/${id}`; //relative link
+  
+    xhttp.open("GET", url, true);
+    xhttp.send();
+ //  console.log("testing 2");
+
+    xhttp.onload = function () {
+    if (xhttp.status == 200) {
+      data = JSON.parse(xhttp.responseText);
+      refreshPage(data);
+    } else {
+      console.error("Error with Data", xhttp.status, xhttp.responseText);
+    }
+  };
+}
+
+function refreshPage(data) {
+
+  document.querySelector("#name").textContent = data.name;
+  document.querySelector("#desc").textContent = data.desc;
+  document.querySelector("#image").src = data.image;
+
+}
+
